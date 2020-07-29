c83763486665608c652058123ba1a72ddcefdf30
Rajesh Raikar <raikar@users.noreply.github.com>
2020-07-29 23:08:41 +0530

    Add files via upload

---
+++ src/main/java/in/javahome/myweb/controller/FirstJava.java
@@ -0,0 +1,13 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello Babu");
+    }
+    public enum UserStatus {
+    PENDING,
+    ACTIVE,
+    INACTIVE,
+    DELETED;
+    public int countryCount;
+    }
+}
